1e44ed7e8fd4b8ad6d4cda4dc88e28e1c3b4e8b1
Israel De León <israel.zacarias@plasticosuni.com>
2022-09-28 10:58:53 -0600

    interpolando varibales

--- app/src/Components/SegundoComponente.js
+++ app/src/Components/SegundoComponente.js
@@ -4,11 +4,13 @@ export const SegundoComponentes = () => {
   return (
     <div>
         <h2>Este es mi segundo componente</h2>
+
        <ul>
         <li>Dato 1</li>
         <li>Dato 2</li>
         <li>Dato 3</li>
        </ul>
+
     </div>
   )
 }
