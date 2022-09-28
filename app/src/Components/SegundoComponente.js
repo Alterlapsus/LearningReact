4a17f9fef3a4e2f668150a23ca70518de6df5483
Israel De León <israel.zacarias@plasticosuni.com>
2022-09-28 10:32:34 -0600

    Se realizan dos componentes

---
+++ app/src/Components/SegundoComponente.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const SegundoComponentes = () => {
+  return (
+    <div>
+        <h2>Este es mi segundo componente</h2>
+       <ul>
+        <li>Dato 1</li>
+        <li>Dato 2</li>
+        <li>Dato 3</li>
+       </ul>
+    </div>
+  )
+}
